30c0aa850f450c8a1b41c453beab147cf74b9cf7
Alex <9136079+arivera247@users.noreply.github.com>
2023-05-19 22:05:18 -0400

    Updated Main class to include main and sequence methods.

--- Fibonacci/src/Main.java
+++ Fibonacci/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Fibonacci Sequence");
+        System.out.println("Enter a value for n to calculate the Fibonacci sequence:");
+        //Output message example: "The nth term of the Fibonacci sequence is y."
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println("The number " + n + " term in the Fibonacci sequence is " + Fibonacci.sequence(n) + ".");
     }
 }
+
+class Fibonacci {
+    static int sequence(int n) {
+        if (n <= 1)
+            return n;
+        return sequence(n - 1) + sequence(n - 2);
+    }
+}
+
+//Reference: https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
